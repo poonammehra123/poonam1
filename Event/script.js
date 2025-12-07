cae646f035961cab1fb41d887548d3d886968609
poonammehra123 <poonammehra862642@gmail.com>
2025-12-07 12:33:08 +0530

    test

--- Event/script.js
+++ Event/script.js
@@ -40,3 +40,62 @@ function Submit(){
     document.getElementById("smartBulb").classList.toggle("on");
  }
 
+ document.getElementById("c1").addEventListener("mouseenter",()=>{
+    fillcolour("Violet")
+ })
+
+  document.getElementById("c1").addEventListener("mouseleave",()=>{
+    fillcolour("white")
+ })
+
+
+ function fillcolour(Color){
+    document.getElementById("rainbowbulb").style.backgroundColor=Color;
+ }
+
+ document.getElementById("c2").addEventListener("mouseenter",()=>{
+    fillcolour("indigo")
+ })
+
+  document.getElementById("c2").addEventListener("mouseleave",()=>{
+    fillcolour("white")
+ })
+ document.getElementById("c3").addEventListener("mouseenter",()=>{
+    fillcolour("blue")
+ })
+
+  document.getElementById("c3").addEventListener("mouseleave",()=>{
+    fillcolour("white")
+ })
+
+ document.getElementById("c4").addEventListener("mouseenter",()=>{
+    fillcolour("green")
+ })
+
+  document.getElementById("c4").addEventListener("mouseleave",()=>{
+    fillcolour("white")
+ })
+
+ document.getElementById("c5").addEventListener("mouseenter",()=>{
+    fillcolour("yellow")
+ })
+
+  document.getElementById("c5").addEventListener("mouseleave",()=>{
+    fillcolour("white")
+ })
+
+ document.getElementById("c6").addEventListener("mouseenter",()=>{
+    fillcolour("orange")
+ })
+
+  document.getElementById("c6").addEventListener("mouseleave",()=>{
+    fillcolour("white")
+ })
+
+ document.getElementById("c7").addEventListener("mouseenter",()=>{
+    fillcolour("red")
+ })
+
+  document.getElementById("c7").addEventListener("mouseleave",()=>{
+    fillcolour("white")
+ })
